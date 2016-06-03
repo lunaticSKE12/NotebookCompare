0b41cc9c01a8c9de6f5a74c50f4764708fd4701f
napong dungduangsasitorn <napong.d@ku.th>
2016-06-03 20:47:14 +0700

    add list notebook

--- app/src/main/java/com/example/lunatic/notebookcompare/NotebookPage.java
+++ app/src/main/java/com/example/lunatic/notebookcompare/NotebookPage.java
@@ -1,15 +1,127 @@
 package com.example.lunatic.notebookcompare;
 
+import android.content.Context;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotebookPage extends AppCompatActivity {
+
+    private ArrayList<String> data = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notebook_page);
+        ListView lv = (ListView) findViewById(R.id.listview);
+        generateListContent();
+        lv.setAdapter(new MyListAdapter(this, R.layout.list_item, data));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(NotebookPage.this, "List item was clicked at " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void generateListContent(){
+        for(int i = 0; i < 55; i++){
+            data.add("This is row number " + i);
+        }
+    }
+
+    public boolean onCreateOptionMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling , menu);
+        return true;
+    }
+
+    public boolean onOptionItemSelected(MenuItem item){
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String>{
+        private int layout;
+
+        public MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
+                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_button);
+                viewHolder.button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                convertView.setTag(viewHolder);
+            }
+            else {
+                mainViewHolder = (ViewHolder) convertView.getTag();
+                mainViewHolder.title.setText(getItem(position));
+            }
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder{
+        ImageView thumbnail;
+        TextView title;
+        Button button;
     }
 
 
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
